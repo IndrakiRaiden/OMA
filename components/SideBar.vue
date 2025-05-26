f17412198067cee0cd0971fa6997a1eb09046b29
IndrakiRaiden <tareasdeneto2200@gmail.com>
2025-05-26 12:01:34 -0600

    hoytodocambio

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -15,6 +15,13 @@
         <span class="nav-text">Servicios</span>
       </NuxtLink>
       
+      <NuxtLink to="/catalogo" class="nav-link" @click="handleLinkClick">
+        <span class="nav-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
+        </span>
+        <span class="nav-text">Catálogo</span>
+      </NuxtLink>
+      
       <NuxtLink to="/industrias" class="nav-link" @click="handleLinkClick">
         <span class="nav-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"/><polyline points="2 17 12 22 22 17"/><polyline points="2 12 12 17 22 12"/></svg>
